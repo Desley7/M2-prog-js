d5e395afbad6130ef755aee1da3f284e14d8eb45
Desley Puttileihalat <d.puttileihalat@icloud.com>
2023-11-23 15:25:12 +0100

    aanpassing

--- 01A/app.js
+++ 01A/app.js
@@ -2,8 +2,24 @@ class App
 {
     runApplication()
     {
-        console.log("hello world");
-        // hier komt de code
+        console.log("hello world")
+            // Variabelen aanmaken
+            let appNaam = "MijnApp";
+            let versienummer = 1; // double
+            let versiedatum = new Date().toLocaleDateString(); // Datum als string
+            let auteur = "Desley puttileihalat";
+            let copyright = "leerling";
+            let distributeur = "leerling";
+            let darkmode = false;
+        
+            // Console.log statements
+            console.log("App Naam:", appNaam);
+            console.log("Versie Nummer:", versienummer);
+            console.log("Versie Datum:", versiedatum);
+            console.log("Auteur:", auteur);
+            console.log("Copyright:", copyright);
+            console.log("Distributeur:", distributeur);
+            console.log("Dark Mode:", darkmode);
     }
 }
 
