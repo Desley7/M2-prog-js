96cbf36532d7877ec22c13b4747c0912a8989f20
Desley Puttileihalat <d.puttileihalat@icloud.com>
2023-11-23 15:03:03 +0100

    b

---
+++ 01A/app.js
@@ -0,0 +1,11 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello world");
+        // hier komt de code
+    }
+}
+
+let app = new App();
+app.runApplication();
---
+++ app.js
@@ -0,0 +1,12 @@
+class App
+{
+    runApplication()
+    {
+        console.log("Hello World!");
+        //code gaat hier
+
+    }
+}
+
+let app = new App();
+app.runApplication();
