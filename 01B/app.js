54399bd4355c1f424c12ca2ea9caccfc73e035ac
Desley Puttileihalat <d.puttileihalat@icloud.com>
2023-11-28 15:13:39 +0100

    k

---
+++ 01B/app.js
@@ -0,0 +1,48 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello world")
+          
+            let appNaam = "MijnApp";
+            let versienummer = 1; // double
+            let versiedatum = new Date().toLocaleDateString(); // Datum als string
+            let auteur = "Desley puttileihalat";
+            let copyright = "leerling";
+            let distributeur = "leerling";
+            let darkmode = false;
+            let isBoolean = true;
+            let textString = "Dit is een string";
+            let aNumber = 42;
+
+            console.log(isBoolean);
+            console.log(textString);
+            console.log(aNumber);
+            console.log("App Naam:", appNaam);
+            console.log("Versie Nummer:", versienummer);
+            console.log("Versie Datum:", versiedatum);
+            console.log("Auteur:", auteur);
+            console.log("Copyright:", copyright);
+            console.log("Distributeur:", distributeur);
+            console.log("Dark Mode:", darkmode);
+
+            class MyClass {
+                constructor() {
+                   
+                    this.classBoolean = false;
+                    this.classString = "Dit is een class string";
+                    this.classNumber = 100;
+                }
+            }
+            
+           
+            const myInstance = new MyClass();
+            console.log(myInstance.classBoolean);
+            console.log(myInstance.classString);
+            console.log(myInstance.classNumber);
+    }
+}
+
+let app = new App();
+app.runApplication();
+
