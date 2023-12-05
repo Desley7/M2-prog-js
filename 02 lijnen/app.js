d97c5a79629563f98b05a31dafbcf2ba65ae158f
Desley Puttileihalat <d.puttileihalat@icloud.com>
2023-12-05 15:29:38 +0100

    l

---
+++ 02 lijnen/app.js
@@ -0,0 +1,36 @@
+class App {
+        runApplication() {
+        console.log("hello world")                     
+        let canvas = document.getElementById("canvasId");
+        console.log(canvas);
+    
+        let g = canvas.getContext("2d");
+        g.fillStyle = "#ff0000";
+        g.fillRect(0,0,canvas.width,canvas.height);
+        
+        g.fillStyle = "#000000";
+        g.fillRect(0,0,10,10);
+
+        g.beginPath();
+        g.fillStyle = "yellow";        
+        g.beginPath()
+        g.moveTo(20,20);
+        g.lineTo(300,300);
+        g.closePath();
+        g.stroke();
+
+        g.beginPath()
+        g.fillStyle = "yellow";
+        g.moveTo(20,20);
+        g.lineTo(300,300);
+        g.lineTo(580,20);
+        g.closePath();
+        g.fill()
+    }
+}
+let app = new App()
+app.runApplication();
+
+
+
+ 
