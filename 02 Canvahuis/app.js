b57c433a4e4d3e86a232ad3dec00d3cc0dccddd7
Desley Puttileihalat <d.puttileihalat@icloud.com>
2023-12-05 15:15:49 +0100

    k

--- 02 Canvahuis/app.js
+++ 02 Canvahuis/app.js
@@ -1,14 +1,20 @@
 class App {
-    runApplication() 
-    {
-        console.log("helloworld");
-        let canvas = document.getElementById("canvasId")
+        runApplication() {
+        console.log("hello world")                     
+        let canvas = document.getElementById("canvasId");
+        console.log(canvas);
+    
         let g = canvas.getContext("2d");
-        g.fillRect(0, 0, 10, 10);
-        console.log(canvas)
-
+        g.fillStyle = "#ff0000";
+        g.fillRect(0,0,canvas.width,canvas.height);
+        
+        g.fillStyle = "#000000";
+        g.fillRect(0,0,10,10);
     }
 }
 let app = new App()
 app.runApplication();
+
+
+
  
